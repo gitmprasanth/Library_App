4715f67ae429e3513aeb1404fb74001983eaf108
PRASANTH M <151944542+gitmprasanth@users.noreply.github.com>
2025-03-14 19:10:29 +0530

    init

---
+++ assets/js/library.js
@@ -0,0 +1,21 @@
+class Library{
+    constructor(){
+        this.books=[]
+    }
+    addBook(book){
+     this.books.push(book)
+    }
+    getBooks(){
+        return this.books
+    }
+    Bookcount(){
+        return this.books.length
+    }
+    removeBook(index){
+       if(index>=0&&index<this.books.length)
+       {
+         this.books.splice(index,1)
+       }
+    }
+
+}
